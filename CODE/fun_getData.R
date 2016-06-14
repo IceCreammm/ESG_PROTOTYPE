9adbc7c6daceb78342140fc993b81b2e91759da4
jason2003wxy <jason.guangyu.wang@gmail.com>
2016-06-14 22:39:04 +0100

    restructure CODE, CSV data initial FLEX DASHBORAD

--- CODE/fun_getData.R
+++ CODE/fun_getData.R
@@ -3,63 +3,32 @@
 library(Quandl)
 library(xts)
 #####################BEGIN: functions to source data################################################################
-GetData.Quandl <- function(dictData, dataExpand=FALSE){
+GetData.Quandl <- function(dictData){
   Check.ExistVarInDF(dictData, c("nameR", "dataSource", "dataId", "type"))
   # Check.StopIf(length(setdiff(dictData$dataSource, c("ECB", "FRED", "EUROSTAT_DSD", "INSEE", "YAHOO")))!=0, 
   #              "data source must be one of 'c(ECB, FRED, EUROSTAT_DSD, INSEE, YAHOO)'")
   Check.StopIf(Size(dictData)[1]!=1, "Must be a single row of data frame!")
   eval(parse(text=paste0(
-    "tmp <- tryCatch(Quandl('", dictData$dataSource, "/", dictData$dataId, "'), error = function(e) {}, warning = function(w) {})"
-    )
+    "tmp <- tryCatch(Quandl('", gsub(" ", "", dictData$dataSource), 
+    "/", gsub(" ", "", dictData$dataId), "'), 
+    error = function(e) {}, warning = function(w) {})"
+    )  
   ))
-  Check.StopIf(class(tmp)[1]=="NULL", paste0(dictData$dataId, "cannot be found in the source of ", dictData$dataSource))
-  tmp$Date <- as.Date(tmp$Date)
+  Check.StopIf(class(tmp)[1]=="NULL", paste0(dictData$dataId, " cannot be found in the source of ", dictData$dataSource))
   Check.StopIf(Size(tmp)[2]!=2, paste0("Expect output should be 2 column df"))
+  tmp[, 1] <- as.Date(tmp[, 1])
   tmp[, 2] <- as.numeric(tmp[, 2])
-  names(tmp) <- c("date", gsub(" ", "", dictData$nameR))
+  names(tmp) <- c("date", "value") #gsub(" ", "", dictData$nameR)
   Check.Unique(tmp$date)
-  tmp <- tmp[match(sort(tmp$date), tmp$date), ] 
-  ##### expand data if type is price, rate or index
-  if (dataExpand) {
-    tmpType <- dictData$type
-    if (toupperNoSpace(dictData$type)%in%c("INDEX", "PRICE")){
-      namNew <- gsub("index", "rate", gsub("price", "rate", gsub(" ", "", dictData$nameR), ignore.case=T))
-      eval(parse(text=paste0(
-        "tmp$", namNew, " <- c(NA, as.numeric(tmp[-1, 2])/as.numeric(head(tmp[, 2], dim(tmp)[1]-1))-1)"
-      )
-      ))
-      tmpType <- c(tmpType, "rate")
-    } else if (toupperNoSpace(dictData$type)=="RATE"){
-      tmpVal <- as.numeric(tmp[, 2])
-      if (sum(is.na(tmpVal))>0){
-        warning(paste0("There are ", sum(sum(is.na(tmpVal))), " NA in ", names(tmp[, 2])), ", which are replace by 0.")
-        tmpVal[is.na(tmpVal)] <- 0
-      }
-      if (toupperNoSpace(dictData$unit)=="%") {
-        tmpVal <- 100 * cumprod(1+ tmpVal/100)
-      } else{
-        stop("Please expand code for unit of rates other than %, here!!!")
-      }
-      eval(parse(text=paste0(
-        "tmp$", gsub("rate", "index", gsub(" ", "", dictData$nameR), ignore.case=T), " <- tmpVal"
-      )
-      ))
-      tmpType <- c(tmpType, "index")
-    } else {
-      warning(paste0(dictData$type, " is not defined (i.e, either price, index or rate). No additional var generated!"))
-    }
+  if (sum(sort(tmp$date)-tmp$date !=0 ) > 0){
+    tmp <- tmp[match(sort(tmp$date), tmp$date), ] 
   }
+  tmp <- tmp[match(sort(tmp$date), tmp$date), ] 
   ## prepare meta data
   dictData$dateBegin <- min(tmp$date)
   dictData$dateEnd <- max(tmp$date)
   dictData$dateFetchData <- as.Date(Sys.time())
-  eval(parse(text=paste0(
-    "desc <- data.frame(cbind(", paste(rep("t(dictData)", Size(tmp)[2]-1), collapse=",") , ")[-1, ], stringsAsFactors=F)"
-  )
-  ))
-  names(desc) <- names(tmp)[-1]
-  row.names(desc) <- row.names(t(dictData))[-1]
-  return(list(dfData=tmp, dataInfo=desc))
+  return(list(dfData=tmp, dataInfo=dictData))
 }
 
 ############## different package
